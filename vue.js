0814100132961e84bf6cc6048780879746c8dcae
unknown <radial1997@gmail.com>
2018-07-04 12:01:07 +0200

    Funcionando con problema con los estilos, quiza tenga que ver con las devtools

---
+++ vue.js
@@ -0,0 +1,15 @@
+
+var vm = new Vue({
+	el: '#selector',
+	data: {
+		mensaje: 'Esto es un string',
+		numero: 2,
+		booleano: true,
+		unArray: [],
+		unObjeto: {
+			nombre: 'Pedrito',
+			apellidos: 'Lopez Peréz'
+		}
+	}
+})
+	
